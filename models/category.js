a241c6f2877c1285f1275dc37c439d5733dec55d
Bartholome Le Bruno <bartholome@MacBook-Pro-de-Bartholome.local>
2018-10-17 17:39:55 +0200

    model category

---
+++ models/category.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes){
+	const category = sequelize.define("category",{
+		Id_category:{
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		nom: {
+			type: DataTypes.VARCHAR,
+			allowNull: false
+		}
+	}, {
+		underscored: true,
+		timestamps: false,
+		freezeTableName: true
+	});
+	return category;
+}
