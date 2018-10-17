dfec4c5e552eab792c0421a6a78f492697f323a5
Sebremy <gauthier-seb@orange.fr>
2018-10-17 17:04:15 +0200

    Added express

---
+++ config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var config = {};
+
+//Webservice configuration
+config.webapp = {};
+config.webapp.dir = process.env.APP_DIR || 'C:/Users/Sebremy/Documents/GitHub/TeachMeAPI/';
+config.webapp.port = '8080';
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const http = require('http');
+'use strict';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const models = require('./models');
+const routes = require('./routes');
+const config = require('./config');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Salut :)\n');
-});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+_startServer();
+
+// INTERNAL
+
+function _startServer() {
+
+    const app = express();
+
+    //RouteManager.attach(app);
+
+    app.listen(8080, function() {
+        console.log('Server started on ' + 8080 + '...');
+    });
+}
