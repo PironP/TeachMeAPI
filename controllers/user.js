5b8af2d6c2fcb8aa14df1e09672039e086c420cb
MBouzeboudja <mahdi.bouzeboudja@etu.parisdescartes.fr>
2018-10-18 16:49:31 +0200

    add delete and update user data

--- controllers/user.js
+++ controllers/user.js
@@ -25,6 +25,7 @@ userController.authotificate = function(email, password){
         return user;
     });
 }
+
 /**
  * @param userParam
  * @returns {Promise<T | never>}
@@ -39,35 +40,60 @@ userController.signIn = function(userParam){
     {
         if (user === undefined || user === null) {
             return User.create({
-                Email: userParam.email,
-                Password: passwordHash.generate(userParam.password),
+                Email:     userParam.email,
+                Password:  passwordHash.generate(userParam.password),
                 LastName:  userParam.lastName,
                 FirstName: userParam.firstName,
-                Tel:       userParam.tel
+                Tel:       userParam.tel,
+                Admin:     userParam.admin
             });
         }
         return false;
     });
 }
 
-userController.update = function(userParam){
+    ²userController.update = function(userParam){
     return User.findOne({
         where: {
             Email: userParam.email,
-        }}
-    )
-        .then (function(user)
-        {
-            if (user === undefined || user === null) {
-                return User.create({
-                    Email: userParam.email,
-                    Password: passwordHash.generate(userParam.password),
-                    LastName:  userParam.lastName,
-                    FirstName: userParam.firstName,
-                    Tel:       userParam.tel
-                });
+        }})
+        .then (function(user) {
+            if (user !== undefined || user !== null) {
+                const options = {}
+                if (userParam.email !== undefined) {
+                    options.Email = userParam.email;
+                }
+                if (userParam.password !== undefined) {
+                    options.Password = userParam.password;
+                }
+                if (userParam.lastName !== undefined) {
+                    options.LastName = userParam.lastName;
+                }
+                if (userParam.firstName !== undefined) {
+                    options.FirstName = userParam.firstName;
+                }
+                if (userParam.tel !== undefined) {
+                    options.Tel = userParam.tel;
+                }
+                if (userParam.admin !== undefined) {
+                    options.Admin = userParam.admin;
+                }
+                return User.update(options, {returning: true, where: {Id_User: user.Id_User}});
             }
             return false;
         });
+}
+
+userController.delete = function(userId) {
+    return User.findOne({
+        where: {
+            Id_User: userId,
+        }})
+            .then(function(user){
+                if (user == undefined || user == null) {
+                    return false;
+                }
+                return User.destroy({where: {Id_User: userId}});
+            });
 }
 module.exports = userController;
