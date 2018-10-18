cdeaec8e1c62931ec1a0e870283db45b64d1de75
Sebremy <gauthier-seb@orange.fr>
2018-10-18 11:12:00 +0200

    Implemented config file for DB info
    added DB info to config file and implemented them in the code

--- config.js
+++ config.js
@@ -7,4 +7,13 @@ config.api = {};
 config.api.dir = process.env.APP_DIR || 'C:/Users/Sebremy/Documents/GitHub/TeachMeAPI/';
 config.api.port = '8080';
 
+//Database configuration
+config.db = {};
+config.db.name = 'u414096900_teach';
+config.db.login = 'u414096900_troot';
+config.db.password = 'rootroot';
+config.db.host = 'sql133.main-hosting.eu';
+config.db.dialect = 'mysql';
+config.db.port = '3306';
+
 module.exports = config;
