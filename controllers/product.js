1dce6a73b3fd2415f15897c9d3e3c72341954107
Bartholome Le Bruno <bartholome@MacBook-Pro-de-Bartholome.local>
2018-10-18 09:01:49 +0200

    getbyidCategory et getbyIDDeposit dans model produit

--- controllers/product.js
+++ controllers/product.js
@@ -15,4 +15,24 @@ productController.getById = function(id) {
   return Product.findAll(options);
 };
 
+productController.getByIdCategory = function(id) {
+  const where = {};
+  const options = {};
+  where.Id_Categorie =  {
+  [Op.eq] : id, 
+  }  
+  options.where = where;
+  return Product.findAll(options);
+};
+
+productController.getByIdDeposit = function(id) {
+  const where = {};
+  const options = {};
+  where.Id_Stockage =  {
+  [Op.eq] : id, 
+  }  
+  options.where = where;
+  return Product.findAll(options);
+};
+
 module.exports = productController;
