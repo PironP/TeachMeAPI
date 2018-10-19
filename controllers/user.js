6e01994cf9cd8b8c15426e73424f535ca999d4ed
MBouzeboudja <mahdi.bouzeboudja@etu.parisdescartes.fr>
2018-10-19 10:15:20 +0200

    fix autontification

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,9 @@ const ModelIndex = require('../models');
 const verifyToken =require('./verifyToken')
 const User = ModelIndex.user;
 var config = require('../config');
-
+// Constants
+const EMAIL_REGEX    = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
 const userController = function(){};
 
 /**
@@ -12,30 +14,36 @@ const userController = function(){};
  * @param password
  * @returns {Promise<T | never>}
  */
-userController.authotificate = function(email, password){
+userController.authontificate = function(email, password){
      return User.findOne({
         where: {
             Email: email,
         }}
     )
-        .then (function(user)
-    {
-        console.log(passwordHash.verify(password, user.Password));
-        if (user === undefined || user === null ||!passwordHash.verify(password, user.Password))
-        {
-            return false;
-        }
-        // create a token
-        var token = verifyToken.generateToken(user);
-        return ({ userID: user.Id_User, token: token });
-    });
-}
+        .then (function(user){
+            console.log(passwordHash.verify(password, user.Password));
+            if (user === undefined || user === null ||!passwordHash.verify(password, user.Password))
+          {
+               return false;
+           }
+           // create a token
+           var token = verifyToken.generateToken(user);
+           return ({ userID: user.Id_User, token: token });
+         });
+};
 
 /**
  * @param userParam
  * @returns {Promise<T | never>}
  */
-userController.signIn = function(userParam){
+userController.signIn = function(req, res){
+    var userParam = req.body;
+    if (!EMAIL_REGEX.test(userParam.email)) {
+        return res.status(400).json({ 'error': 'email is not valid' });
+    }
+    if (!PASSWORD_REGEX.test(userParam.password)) {
+        return res.status(400).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least)' });
+    }
     return User.findOne({
         where: {
             Email: userParam.email,
@@ -55,39 +63,39 @@ userController.signIn = function(userParam){
         }
         return false;
     });
-}
+};
 
-    userController.update = function(userParam){
-    return User.findOne({
-        where: {
-            Email: userParam.email,
-        }})
-        .then (function(user) {
-            if (user !== undefined || user !== null) {
-                const options = {}
-                if (userParam.email !== undefined) {
-                    options.Email = userParam.email;
-                }
-                if (userParam.password !== undefined) {
-                    options.Password = userParam.password;
-                }
-                if (userParam.lastName !== undefined) {
-                    options.LastName = userParam.lastName;
-                }
-                if (userParam.firstName !== undefined) {
-                    options.FirstName = userParam.firstName;
-                }
-                if (userParam.tel !== undefined) {
-                    options.Tel = userParam.tel;
-                }
-                if (userParam.admin !== undefined) {
-                    options.Admin = userParam.admin;
-                }
-                return User.update(options, {returning: true, where: {Id_User: user.Id_User}});
+userController.update = function(userParam){
+return User.findOne({
+    where: {
+        Email: userParam.email,
+    }})
+    .then (function(user) {
+        if (user !== undefined || user !== null) {
+            const options = {}
+            if (userParam.email !== undefined) {
+                options.Email = userParam.email;
             }
-            return false;
-        });
-}
+            if (userParam.password !== undefined) {
+                options.Password = userParam.password;
+            }
+            if (userParam.lastName !== undefined) {
+                options.LastName = userParam.lastName;
+            }
+            if (userParam.firstName !== undefined) {
+                options.FirstName = userParam.firstName;
+            }
+            if (userParam.tel !== undefined) {
+                options.Tel = userParam.tel;
+            }
+            if (userParam.admin !== undefined) {
+                options.Admin = userParam.admin;
+            }
+            return User.update(options, {returning: true, where: {Id_User: user.Id_User}});
+        }
+        return false;
+    });
+};
 
 userController.delete = function(userId) {
     return User.findOne({
@@ -100,5 +108,5 @@ userController.delete = function(userId) {
                 }
                 return User.destroy({where: {Id_User: userId}});
             });
-}
+};
 module.exports = userController;
--- routes/user.js
+++ routes/user.js
@@ -10,20 +10,18 @@ userRouter.post('/login', function(req,res){
     if(req.body.email === undefined || req.body.password === undefined){
         return res.status(400).end();
     }
-    UserController.authotificate(req.body.email, req.body.password )
+    UserController.authontificate(req.body.email, req.body.password )
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Email or password is incorrect' }))
         .catch(err => {
             console.log(err);
             res.status(400).end();
         });
 });
-
 userRouter.post('/signIn', function(req,res){
-    console.log(req);
     if(req.body.email === undefined || req.body.password === undefined || req.body.lastName === undefined || req.body.firstName === undefined || req.body.tel === undefined){
         return res.status(400).json({ message: 'All fields are required' }).end();
     }
-    UserController.signIn(req.body)
+    UserController.signIn(req, res)
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Email already exits' }))
         .catch(err => {
             console.log(err);
